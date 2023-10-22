a6d901360a84797995dfb800dd16b2567c851029
gustavomarsan <gustavo.mtz012@gmial.com>
2023-10-22 12:38:02 -0600

    Put method done

--- backend/todo/views.py
+++ backend/todo/views.py
@@ -16,3 +16,16 @@ def todo_endpoint(request: HttpRequest) -> JsonResponse:
         done = data["done"]
         new_todo = Todo.objects.create(title=title, done=done)
         return JsonResponse(model_to_dict(new_todo))
+
+
+@csrf_exempt
+def todo_update_del(request: HttpRequest, id: int) -> JsonResponse:
+    if request.method == "PUT":
+        data = json.loads(request.body)
+        title = data["title"]
+        done = data["done"]
+        todo = Todo.objects.get(id=id)
+        todo.title = title
+        todo.done = done
+        todo.save()
+        return JsonResponse(model_to_dict(todo))
