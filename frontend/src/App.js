12cf39f1664030d1d9f7e3e025c6feb4030e6872
Dario Martinez <dariom@lyft.com>
2023-10-29 14:01:46 -0600

    Create todo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,31 @@
 import Todo from "./Todo";
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
+  async function createTodo() {
+    const response = await fetch("http://localhost:3000/todos", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ title: newTodo })
+    })
+    const newTodoResponse = await response.json()
+
+    setTodos([...todos, newTodoResponse])
+    setNewTodo("")
+  }
+
   return (
     <div>
       <header>
         The TODO App
       </header>
-      <Todo task={{ title: "My first task", done: false }} />
+      <TextBox value={newTodo} onChange={setNewTodo} />
+      <button onClick={createTodo}>Save</button>
+      {todos.map((todo) => (
+        <Todo task={todo} />
+      ))}
     </div>
   );
 }
