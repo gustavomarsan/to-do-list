694b0dee987a14d313230c1983b3eb4267153f13
dariomtz <gusdamtzr@gmail.com>
2023-10-30 19:26:06 -0600

    Fix errors

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,25 @@
 import Todo from "./Todo";
+import TextBox from "./utils/TextBox";
+import { useState } from "react";
+
 function App() {
   const [todos, setTodos] = useState([]);
   const [newTodo, setNewTodo] = useState("");
   async function createTodo() {
-    const response = await fetch("http://localhost:3000/todos", {
+    const response = await fetch("http://localhost:8000/todo/", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
       },
       body: JSON.stringify({ title: newTodo })
     })
+
+    if (response.status !== 201) {
+      console.log("Error creating todo")
+      return
+    }
     const newTodoResponse = await response.json()
+    console.log(newTodoResponse)
 
     setTodos([...todos, newTodoResponse])
     setNewTodo("")
@@ -21,10 +30,10 @@ function App() {
       <header>
         The TODO App
       </header>
-      <TextBox value={newTodo} onChange={setNewTodo} />
+      <TextBox value={newTodo} setValue={setNewTodo} />
       <button onClick={createTodo}>Save</button>
       {todos.map((todo) => (
-        <Todo task={todo} />
+        <Todo key={todo.id} task={todo} />
       ))}
     </div>
   );
