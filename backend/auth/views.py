7156b27e8cc8710f2bcb0a297d9d9949373f589a
gustavomarsan <105552247+gustavomarsan@users.noreply.github.com>
2023-11-19 13:50:31 -0600

    Add users (#24)
    Co-authored-by: gustavomarsan <gustavo.mtz012@gmial.com>

--- backend/auth/views.py
+++ backend/auth/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpRequest, JsonResponse, HttpResponse
+from django.http import HttpRequest, HttpResponse
+import json
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.models import User
+from auth.decorators import error_handler_decorator
 
 
 # Create your views here.
 
 
 @csrf_exempt
-def auth(request: HttpRequest, *args) -> JsonResponse:
-    print("Hello world")
-    return HttpResponse("Hello world")
+@error_handler_decorator
+def sign_up(request: HttpRequest) -> HttpResponse:
+    if request.method == "POST":
+        data = json.loads(request.body)
+        username = data["username"]
+        pasword = data["password"]
+        email = data["email"]
+        first_name = data["first_name"]
+        last_name = data["last_name"]
+        new_user = User.objects.create_user(username, pasword, email)
+        new_user.first_name = first_name
+        new_user.last_name = last_name
+        new_user.save()
+        return HttpResponse(status=201)
